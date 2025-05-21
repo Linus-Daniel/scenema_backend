6daed10bf41183ba9665b15c78e65cef07f34d96
Linus-Daniel <vandulinus@gmail.com>
2025-05-21 04:56:42 +0100

    first commit

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app');
 const { initializeSocket } = require('./config/socket');
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
